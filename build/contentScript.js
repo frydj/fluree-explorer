10f85a632c25cf15807b0df03efab940066c3f04
Derek Fry <45840262+frydj@users.noreply.github.com>
2022-12-19 15:13:46 -0500

    starting react

--- build/contentScript.js
+++ build/contentScript.js
@@ -11,6 +11,13 @@
   };
 
   // add container to body
+  const body = document.getElementsByTagName('body')[0];
+
+  let roott = document.createElement('div');
+  roott.setAttribute('id', 'roott');
+  roott.innerHTML = 'hello whirl';
+
+  body.appendChild(roott);
 
   // get JSON-LD element(s)
   let foundJson = document.querySelectorAll('[type="application/ld+json"]');
