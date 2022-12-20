22b8c4714ab809bc66db2128229de13ab8c63bef
Derek Fry <45840262+frydj@users.noreply.github.com>
2022-12-20 14:28:12 -0500

    searchbar continued

--- src/components/Explorer/Explorer.jsx
+++ src/components/Explorer/Explorer.jsx
@@ -5,21 +5,41 @@ const Explorer = (data) => {
   const [url, setUrl] = useState('https://flur.ee');
   const searchBar = useRef();
 
-  const navigate = () => {
-    setUrl(searchBar.current.value);
+  const navigate = (e) => {
+    let address = searchBar.current.value;
+    if (address.indexOf('://') === -1) {
+      address = 'https://' + address;
+      setSearchBar(address);
+    }
+    setUrl(address);
+    e.target.blur();
+  };
+
+  const setSearchBar = (newValue) => {
+    searchBar.current.value = newValue;
+  };
+
+  const selectContents = (e) => {
+    e.target.select();
   };
 
   const checkKey = (e) => {
     if (e.key === 'Enter') {
       console.log('enter was pressed');
-      navigate();
+      navigate(e);
     }
   };
 
   return (
     <div id="explorer-container">
       <div id="search-bar-container">
-        <input onKeyDown={checkKey} ref={searchBar} id="search-bar" autoFocus />
+        <input
+          onKeyDown={checkKey}
+          onFocus={selectContents}
+          ref={searchBar}
+          id="search-bar"
+          autoFocus
+        />
       </div>
       <iframe src={url} title="fluree-explorer-window"></iframe>
     </div>
